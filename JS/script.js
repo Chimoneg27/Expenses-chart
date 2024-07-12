2d0dc5df2a5746aae64865cd68a4d4f0fd99411e
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-07-12 03:37:10 +0200

    get data from json file

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,12 @@
-// this file processes the json data
+// http://localhost:8000/data.json
+let days = []
+let prices
+
+fetch('http://localhost:8000/data.json')
+    .then(response => response.json())
+    .then((data) =>
+        {for(let i = 0; i < data.length;  i++) {
+            days.push(data[i].day)
+            prices.push(data[i].amount)
+        }}
+    )
