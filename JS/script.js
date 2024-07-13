e593ba44eecde8d45f4f1b23fcb5f0444483ae1e
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-07-13 19:13:51 +0200

    fetch data form json file

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,13 @@ const url = 'https://raw.githubusercontent.com/Chimoneg27/Expenses-chart/main/da
 let days = []
 let prices = []
 
+fetch(url)
+    .then(res => res.json())
+    .then(data => 
+        {for(let i = 0; i < data.length; i++) {
+            days.push(data[i].day)
+            prices.push(data[i].amount)
+        }
+    console.log(days)
+    }
+    )
